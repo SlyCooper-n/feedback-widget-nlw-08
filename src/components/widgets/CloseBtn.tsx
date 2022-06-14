aa05a63966ca59da92227f3ab6f6dcda83b86865
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-14 13:53:24 -0300

    resized icon

--- src/components/widgets/CloseBtn.tsx
+++ src/components/widgets/CloseBtn.tsx
@@ -7,7 +7,7 @@ export const CloseBtn = () => {
       title="Close feedback form"
       className="absolute top-5 right-5 text-zinc-400 hover:text-zinc-100"
     >
-      <X weight="bold" className="w-4 aspect-square" />
+      <X weight="bold" className="w-4 h-4" />
     </Popover.Button>
   );
 };
