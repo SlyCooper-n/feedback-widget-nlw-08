e61f3a0ff89e024f9ccea3544bbe825f97f8269e
Gabriel <gabrielvitor.frasao@gmail.com>
2022-06-26 20:57:03 -0300

    prisma integration and SQLite db created

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,22 @@
+import { Request, Response } from "express";
+import { prisma } from "./prisma";
+
 const express = require("express");
 
 const app = express();
+
+// middleware
+app.use(express.json());
+
+app.post("/", async (req: Request, res: Response) => {
+  const feedback = await prisma.feedback.create({
+    data: {
+      ...req.body,
+    },
+  });
+
+  return res.status(201).json({ data: feedback, message: "Feedback received" });
+});
 
 app.listen(3333, () => {
   console.log(
