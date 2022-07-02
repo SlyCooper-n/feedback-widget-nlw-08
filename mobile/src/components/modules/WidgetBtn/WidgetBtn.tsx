461921951801d9294dc2190c5f20ca0b4ab568b7
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-02 19:17:51 -0300

    widget buttom component created

---
+++ mobile/src/components/modules/WidgetBtn/WidgetBtn.tsx
@@ -0,0 +1,36 @@
+import BottomSheet from "@gorhom/bottom-sheet";
+import { ChatTeardropDots } from "phosphor-react-native";
+import { useRef } from "react";
+import { TouchableOpacity } from "react-native";
+import { theme } from "../../../theme";
+import { Steps } from "../Steps";
+import { styles } from "./WidgetBtn.style";
+
+export const WidgetBtn = () => {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function openButtomSheet() {
+    bottomSheetRef.current?.expand();
+  }
+
+  return (
+    <>
+      <TouchableOpacity onPress={openButtomSheet} style={styles.button}>
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[0.01, 280]}
+        handleIndicatorStyle={styles.indicator}
+        backgroundStyle={styles.modal}
+      >
+        <Steps />
+      </BottomSheet>
+    </>
+  );
+};
