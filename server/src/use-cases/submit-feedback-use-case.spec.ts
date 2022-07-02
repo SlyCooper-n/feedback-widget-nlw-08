a5bc1daee7e30d48c182520722ada648f34c04ef
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-02 14:38:50 -0300

    async/await added into it functions and vitest spies

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,24 +1,31 @@
-import { describe, expect, it } from "vitest";
+// import { describe, expect, it } from "vitest";
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
+// vitest spies
+const createFeedbackSpy = vitest.fn();
+const sendMailSpy = vitest.fn();
+
 const submitFeedback = new SubmitFeedbackUseCase(
-  { create: () => Promise.resolve() },
-  { sendMail: () => Promise.resolve() }
+  { create: createFeedbackSpy },
+  { sendMail: sendMailSpy }
 );
 
 describe("Submit Feedback", () => {
-  it("should submit a feedback", () => {
-    expect(
+  it("should submit a feedback", async () => {
+    await expect(
       submitFeedback.run({
         type: "BUG",
         comment: "This is a comment",
         screenshot: "data:image/png/base64,example.com/screenshot.png",
       })
     ).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
   });
 
-  it("should throw an error if type or comment is not provided", () => {
-    expect(
+  it("should throw an error if type or comment is not provided", async () => {
+    await expect(
       submitFeedback.run({
         type: "",
         comment: "",
@@ -26,8 +33,8 @@ describe("Submit Feedback", () => {
     ).rejects.toThrow("Type and comment are required");
   });
 
-  it("should throw an error if screenshot is not a valid URL", () => {
-    expect(
+  it("should throw an error if screenshot is not a valid URL", async () => {
+    await expect(
       submitFeedback.run({
         type: "IDEA",
         comment: "This is another comment",
