51bf9d0798da88ef769e92218ce696a4cf60ce54
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-02 19:17:11 -0300

    steps component created

---
+++ mobile/src/components/modules/Steps/Steps.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { Copyright } from "../../widgets/Copyright";
+import { styles } from "./Steps.style";
+
+export const Steps = () => {
+  return (
+    <View style={styles.container}>
+      <Copyright />
+    </View>
+  );
+};
