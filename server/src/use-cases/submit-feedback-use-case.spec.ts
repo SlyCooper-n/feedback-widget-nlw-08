2a3912e7d7d40a458df4338dc94bd3234d5eea02
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-02 13:32:26 -0300

    testing file for submit feedback use case

---
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -0,0 +1,34 @@
+// import { describe, expect, it } from "vitest";
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+describe("Submit Feedback", () => {
+  it("should submit a feedback", () => {
+    const submitFeedback = new SubmitFeedbackUseCase(
+      { create: () => Promise.resolve() },
+      { sendMail: () => Promise.resolve() }
+    );
+
+    expect(
+      submitFeedback.run({
+        type: "BUG",
+        comment: "This is a comment",
+        screenshot: "data:image/png/base64,example.com/screenshot.png",
+      })
+    ).resolves.not.toThrow();
+  });
+
+  it("should throw an error if screenshot is not a valid URL", () => {
+    const submitFeedback = new SubmitFeedbackUseCase(
+      { create: () => Promise.resolve() },
+      { sendMail: () => Promise.resolve() }
+    );
+
+    expect(
+      submitFeedback.run({
+        type: "IDEA",
+        comment: "This is another comment",
+        screenshot: "example.com/screenshot.png",
+      })
+    ).rejects.toThrow("Screenshot must be a valid URL");
+  });
+});
