4dc101816ae1a952c37cc116ba02bc8ef4eacdf5
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-02 13:30:33 -0300

    submit feedback use case following SOLID principles

---
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -0,0 +1,63 @@
+import { MailAdapter } from "@/adapters";
+import { FeedbacksRepository } from "@/repositories";
+
+interface SubmitFeedbackUseCaseRequest {
+  type: string;
+  comment: string;
+  screenshot?: string;
+}
+
+export class SubmitFeedbackUseCase {
+  constructor(
+    private readonly feedbacksRepository: FeedbacksRepository,
+    private readonly mailAdapter: MailAdapter
+  ) {}
+
+  async run(request: SubmitFeedbackUseCaseRequest): Promise<void> {
+    const { type, comment, screenshot } = request;
+
+    /*
+    ! WRONG WAY TO DO IT, not following the SOLID principles
+
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+
+    await prismaFeedbacksRepository.create({
+      type,
+      comment,
+      screenshot,
+    });
+
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
+
+    await nodemailerMailAdapter.sendMail({
+      subject: "Feedback received",
+      body: [].join("\n"),
+    })
+    */
+
+    // * RIGHT WAY TO DO IT, following the SOLID principles
+
+    if (screenshot && !screenshot.startsWith("data:image/png/base64")) {
+      throw new Error("Screenshot must be a valid URL");
+    }
+
+    await this.feedbacksRepository.create({
+      type,
+      comment,
+      screenshot,
+    });
+
+    await this.mailAdapter.sendMail({
+      subject: "Feedback received",
+      body: [
+        `<html>`,
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+        `<p><span style="font-weight: bold;">Feedback Type</span>: ${type}</p>`,
+        `<p><span style="font-weight: bold;">Comment</span>: ${comment}</p>`,
+        `</div>`,
+        `</html>`,
+      ].join("\n"),
+      textBody: [`Feedback Type: ${type}`, `Comment: ${comment}`].join("\n"),
+    });
+  }
+}
