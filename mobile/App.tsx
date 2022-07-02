ca16dc8875a8a8bfe52dbf89c36cca4015dcd596
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-02 19:16:52 -0300

    loading fonts before rendering app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,39 @@
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import { useFonts } from "expo-font";
+import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import "react-native-gesture-handler";
+import WidgetBtn from "./src/components/modules/WidgetBtn";
+import { theme } from "./src/theme";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+  async () => {
+    await preventAutoHideAsync();
+  };
 
-      <Text>Open up App.tsx to start working on your app!</Text>
+  const [fontLoaded, error] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+  });
+
+  if (!fontLoaded) return null;
+  if (error) return <Text>Error: {error.message}</Text>;
+
+  async () => {
+    await hideAsync();
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+      }}
+    >
+      <StatusBar style="light" backgroundColor="transparent" translucent />
+
+      <WidgetBtn />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
