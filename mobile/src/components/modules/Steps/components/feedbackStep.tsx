0be369caec86027bc844dfa1ecb954431b6b98cf
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-03 18:21:49 -0300

    title added

--- mobile/src/components/modules/Steps/components/feedbackStep.tsx
+++ mobile/src/components/modules/Steps/components/feedbackStep.tsx
@@ -1,14 +1,26 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { FeedbackType } from "../../../../utils/feedbackContext";
 import { feedbackTypes } from "../../../../utils/feedbackTypes";
 import { Option } from "../../../widgets/Option";
 import { styles } from "../Steps.style";
 
 export const FeedbackStep = () => {
   return (
-    <View style={styles.options}>
-      {Object.entries(feedbackTypes).map(([key, value]) => {
-        return <Option key={key} title={value.title} image={value.image} />;
-      })}
-    </View>
+    <>
+      <Text style={styles.title}>Leave your Feedback</Text>
+
+      <View style={styles.options}>
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return (
+            <Option
+              key={key}
+              title={value.title}
+              image={value.image}
+              type={key as FeedbackType}
+            />
+          );
+        })}
+      </View>
+    </>
   );
 };
