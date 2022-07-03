313b6a9fdaf1ddf1d20737133d22b2ced7d20634
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-03 17:08:49 -0300

    option component manage state on press

--- mobile/src/components/widgets/Option/Option.tsx
+++ mobile/src/components/widgets/Option/Option.tsx
@@ -1,14 +1,21 @@
 import { Image, ImageProps, Text, TouchableOpacity } from "react-native";
+import { FeedbackType, useFeedback } from "../../../utils/feedbackContext";
 import { styles } from "./Option.style";
 
 interface OptionProps {
   title: string;
   image: ImageProps;
+  type: FeedbackType;
 }
 
-export const Option = ({ title, image }: OptionProps) => {
+export const Option = ({ title, image, type }: OptionProps) => {
+  const { selectFeedbackType } = useFeedback();
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => selectFeedbackType(type)}
+    >
       <Image source={image} style={styles.image} />
 
       <Text style={styles.title}>{title}</Text>
