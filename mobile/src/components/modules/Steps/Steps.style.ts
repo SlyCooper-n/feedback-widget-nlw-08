d81795730a538a5fbd98f0f86b03b4ac269a23c2
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-03 13:43:48 -0300

    feedback options step finished

--- mobile/src/components/modules/Steps/Steps.style.ts
+++ mobile/src/components/modules/Steps/Steps.style.ts
@@ -1,7 +1,27 @@
 import { StyleSheet } from "react-native";
+import { theme } from "../../../theme";
 
 export const styles = StyleSheet.create({
   container: {
+    height: "100%",
+    paddingVertical: 8,
+    justifyContent: "space-between",
     alignItems: "center",
+  },
+
+  title: {
+    marginBottom: 32,
+
+    fontSize: 20,
+    fontFamily: theme.fonts.medium,
+    color: theme.colors.text_primary,
+  },
+
+  options: {
+    width: "100%",
+    marginBottom: 48,
+
+    flexDirection: "row",
+    justifyContent: "center",
   },
 });
--- mobile/src/components/modules/Steps/Steps.tsx
+++ mobile/src/components/modules/Steps/Steps.tsx
@@ -1,10 +1,20 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { feedbackTypes } from "../../../utils/feedbackTypes";
 import { Copyright } from "../../widgets/Copyright";
+import { Option } from "../../widgets/Option";
 import { styles } from "./Steps.style";
 
 export const Steps = () => {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Leave your feedback</Text>
+
+      <View style={styles.options}>
+        {Object.entries(feedbackTypes).map(([key, value]) => {
+          return <Option key={key} title={value.title} image={value.image} />;
+        })}
+      </View>
+
       <Copyright />
     </View>
   );
