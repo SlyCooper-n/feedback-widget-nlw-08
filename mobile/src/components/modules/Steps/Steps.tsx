7d1c7dae8c45ac23609a1e0144e89e2ae96f3cbe
Gabriel <gabrielvitor.frasao@gmail.com>
2022-07-03 18:26:05 -0300

    steps organized in order

--- mobile/src/components/modules/Steps/Steps.tsx
+++ mobile/src/components/modules/Steps/Steps.tsx
@@ -1,18 +1,21 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { useFeedback } from "../../../utils/feedbackContext";
 import { Copyright } from "../../widgets/Copyright";
 import { FeedbackStep } from "./components/FeedbackStep";
 import { FormStep } from "./components/FormStep";
+import { SuccessStep } from "./components/SuccessStep";
 import { styles } from "./Steps.style";
 
 export const Steps = () => {
-  const { feedbackType } = useFeedback();
+  const { feedbackType, feedbackSent } = useFeedback();
 
   return (
     <View style={styles.container}>
-      {!feedbackType && <Text style={styles.title}>Leave your Feedback</Text>}
+      {!feedbackType && <FeedbackStep />}
 
-      {!feedbackType ? <FeedbackStep /> : <FormStep />}
+      {feedbackType && !feedbackSent && <FormStep />}
+
+      {feedbackSent && <SuccessStep />}
 
       <Copyright />
     </View>
